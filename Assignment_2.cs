785dce8a5b6f9d7b442119e6800eb343118c37c2
pankaj3376 <71892082+pankaj3376@users.noreply.github.com>
2023-05-09 13:00:20 +0530

    Add files via upload

---
+++ Assignment_2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+
+class Assignment_08_05_2023
+{
+    public static void Main()
+    {
+        
+        bool flag = true;
+        while (flag)
+        {
+            int num;
+            Console.WriteLine("Enter 1 for factorial.");
+            Console.WriteLine("Enter 2 for fibonacci series.");
+            Console.WriteLine("Enter 3 to check prime number.");
+            Console.WriteLine("Enter the  number from above.");
+            num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    int fact;
+                    int factriol;
+                    factriol = 1;
+                    Console.WriteLine("Enter number for factorial");
+                    fact = Convert.ToInt32(Console.ReadLine());
+                    for (int i = fact; i > 0; i--)
+                    {
+                        factriol *= i;
+                        //Console.WriteLine(factriol);
+                    }
+                    Console.WriteLine("Factorial of " + fact + " is: " + factriol);
+                    break;
+
+                case 2:
+                    int num1 = 0, num2 = 1, num3, number;
+                    Console.WriteLine("Enter a number for fibonacci series");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("fibonacci series of " + number + " is: " + num1 + " " + num2 + " ");
+                    
+                    for (int i = 2; i < number; ++i)
+                    {
+                        num3 = num1 + num2;
+                        Console.Write(num3 + " ");
+
+                        num1 = num2;
+                        num2 = num3;
+                    }
+                    Console.WriteLine("");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter a number is to check it's prime or not.");
+                    int pnum = Convert.ToInt32(Console.ReadLine());
+                    bool Isprime = true;
+
+                    for (int i = 2; i < pnum / 2; ++i)
+                    {
+                        if (pnum % i == 0)
+                        {
+                            Isprime = false;
+                            break;
+                        }
+
+                    }
+                    if (Isprime)
+                    {
+                        Console.WriteLine("{0} is a prime number.", pnum);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} is not a prime number.", pnum);
+                    }
+                    break;
+
+                default:
+
+                    if (num > 3 || num <= 0)
+                    {
+                        Console.WriteLine("Please Enter valid case number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NOT FOUND !!");
+                    }
+                    break;
+
+
+
+            }
+
+            Console.WriteLine("do you want to continue.press Y for yes and N for no.");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if(ch!='Y' && ch!='y')
+            {
+                flag= false;
+            }
+            
+
+        }
+    }
+}
---
+++ Assignment_3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class studet
+{
+    int ID;
+    string Name;
+    int Amount;
+
+    studentDetails obj = new studentDetails();
+
+    public int SID { get; set; }
+    public string SNAME { get; set; }
+    //public int SAMOUNT { get; set; }
+    public int SAMOUNT
+    {
+        get
+        {
+            return (int)this.Amount;
+        }
+        set
+        {
+            this.Amount = value;
+            if (value < 10000)
+            {
+                obj.notGoForShopping();
+            }
+            else
+            {
+                obj.goForShopping();
+            }
+        }
+    }
+}
+
+class studentDetails
+{
+    public void goForShopping()
+    {
+        Console.WriteLine("You can go for shopping");
+    }
+    public void notGoForShopping()
+    {
+        Console.WriteLine("You can't go for shopping");
+    }
+
+    public void stdDetaild()
+    {
+        studet std = new studet();
+        std.SID = 101;
+        std.SNAME = "Pankaj Priyadarshi";
+        std.SAMOUNT = 20000;
+        Console.WriteLine(std.SID+" "+ std.SNAME+" "+ std.SAMOUNT);
+    }
+}
+
+class result
+{
+    public static void Main(string[] args)
+    {
+        studentDetails std = new studentDetails();
+        std.stdDetaild();
+    }
+}
