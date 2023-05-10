6145a45de9ae08ebedd6eed22bf9943fcbe7d0ac
pankaj3376 <71892082+pankaj3376@users.noreply.github.com>
2023-05-10 14:12:05 +0530

    Add files via upload

---
+++ Indexer_10_05_23.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Employee
+{
+    object[] Emp = new object[4];
+
+    public object this[int i]
+    {
+        get
+        {
+            return Emp[i];
+        }
+        set
+        {
+            Emp[i] = value;
+        }
+    }
+}
+
+class empDetails
+{
+    public static void Main(string[] args)
+    {
+        Employee obj = new Employee();
+        obj[0] = 101;
+        obj[1] = "Pankaj";
+        obj[2] = 27;
+        obj[3] = 50000;
+        Console.WriteLine(" ID: " + obj[0] + "\n Name: " + obj[1] + "\n Age: " + obj[2] + "\n Salary: " + obj[3]);
+    }
+}
